181064a2371b4fe167f8a5bb8eeea8c0c271972a
bconstanzo <bconstanzo@users.noreply.github.com>
2020-07-21 00:19:32 -0300

    added: .raw_data, .size and .attributes properties

--- haruspex/fat32.py
+++ haruspex/fat32.py
@@ -25,6 +25,14 @@ ATTRIBUTES = {
 FILENAME_CHARS = set(b"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ_-")
 
 
+def _fat32_time(bytes_, mili=b"\x00"):
+    """
+    Receives date + time bytes in `bytes`, and returns a datetime object.
+    Checks for pathological 0xffff patterns
+    """
+    raw_date, raw_time, = struct.unpack(">HH", bytes_)
+    seconds += mili[0] / 200
+
 
 class FileRecord:
     def __init__(self, data):
@@ -32,7 +40,6 @@ class FileRecord:
         self._name       = b""
         self._ext        = b""
         self._size       = -1
-        self._attributes = {k: False for k in attributes}
         self._attributes = {k: False for k in ATTRIBUTES}
         self._cluster    = -1
         self._ts_create  = None
@@ -40,8 +47,17 @@ class FileRecord:
         self._ts_mod     = None
         # that's all for real attributes of a file record
         self.verbose     = False
+        self._parse()
+    
     # The Properties
     # (these could be done with metaclasses, but...)
+    @property
+    def raw_data(self):
+        return self._raw_data
+    
+    @raw_data.setter
+    def raw_data(self, value):
+        pass  # let's make it read-only for the moment
 
     @property
     def name(self):
@@ -73,16 +89,36 @@ class FileRecord:
         ext = ext[:3]
         self._ext = ext
     
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self, value):
+        if value > 0xffffffff:
+            value = 0xffffffff
+        self._size = value
+    
+    @property
+    def attributes(self):
+        return self._attributes
+    
+    @attributes.setter
+    def attributes(self, value):
+        # let's cleanup in case the given dict has some extra keys
+        value = {k:v for k, v in value.items() if k in ATTRIBUTES}
+        self._attributes.update(value)
+    
     def __repr__(self):
         if self.verbose:
             return (
                 f"< DirectoryEntry: {self.name}.{self.ext}\n"
-                f"  {'cluster':12}:{self.cluster:>12}\n" 
                 f"  {'size':12}:{self.size:>12}\n" 
+                f"  {'attributes':12}:{self._attrs2str():>12}\n" 
+                f"  {'cluster':12}:{self.cluster:>12}\n" 
                 f"  {'created':12}:{self.created:>12}\n" 
                 f"  {'modified':12}:{self.modified:>12}\n" 
                 f"  {'accessed':12}:{self.accessed:>12}\n" 
-                f"  {'attributes':12}:{self._attrs2str():>12}\n" 
                 f">"
             )
         return f"< DirectoryEntry: {self.name}.{self.ext}>"
@@ -93,6 +129,12 @@ class FileRecord:
             for (k, v) in self._attributes.items()
         ]
         return "".join(ret)
+    
+    def _parse(self):
+        """
+        Parses self._raw_data and updates the properties
+        """
+        pass
 
 
 class Filesystem:

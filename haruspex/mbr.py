c6dc72698a3592e2259dd70ae93769ef8cc94d85
bconstanzo <bconstanzo@users.noreply.github.com>
2020-07-24 02:48:31 -0300

    change: refactored parsing of the Table and Records

--- haruspex/mbr.py
+++ haruspex/mbr.py
@@ -22,9 +22,9 @@ class Record:
         """
         self._raw_data = data
         self.bootable  = False
-        self.chs_start = 0x0
+        self.chs_start = (0x0, 0x0, 0x0)
         self.type      = 0x0
-        self.chs_end   = 0x0
+        self.chs_end   = (0x0, 0x0, 0x0)
         self.lba_start = 0x0
         self.size      = 0
         # after setting the attributes for the instance, we parse the raw data
@@ -39,11 +39,18 @@ class Record:
         Parses self._raw_data and uses it to modifiy/complete the internal state
         of Record.
         """
+        def _chs(value):
+            head, sector, cylinder = struct.unpack("<3B", value)
+            cylinder = ((sector & 0b11000000) << 2) | cylinder
+            sector   =   sector & 0b00111111
+            return (cylinder, head, sector)
+
         data = self._raw_data
         self.bootable   = data[0] == 0x80
-        self.chs_start  = data[1:4]
+        # CHS addresses should be 0 in any recent MBR, stil...
+        self.chs_start  = _chs(data[1:4])
         self.type       = self.partition_types.get(data[4], "Unknown")
-        self.chs_end    = data[5:8]
+        self.chs_end    = _chs(data[5:8])
         self.lba_start, = struct.unpack("<I", data[8:12])
         self.size,      = struct.unpack("<I", data[12:16])
 
@@ -58,7 +65,7 @@ class Table:
         """
         self._raw_data = data
         self.partitions = []
-        self.parse()
+        self._parse()
     
     def __repr__(self):
         ret  = [f"< Partition Table @ {id(self)} >"]
@@ -67,13 +74,18 @@ class Table:
         )
         return "\n".join(ret)
     
-    def parse(self):
+    def _parse(self):
         """
         Parses the raw data passed during initialization. Does not return, but
         changes the self.partitions list.
         """
-        data = self._raw_data
-        p_parts = [data[446 + i * 16: 446 + i * 16 + 16] for i in range(4)]
-        p_parts = filter(lambda x: x != b"\x00" * 16, p_parts)
+        data = self._raw_data[446:]
+        p_parts = filter(
+            lambda x: x != b"\x00" * 16,  # filter out empty partitions
+            (data[i * 16: i * 16 + 16] for i in range(4))
+        )
         self.partitions = [Record(p) for p in p_parts]
+        # this implementation is "good enough", but not thorough
+        # -- the legacy files have a larger implementation that includes
+        #    extended partitions parsing
 

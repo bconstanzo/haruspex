0c2b358b9d10fdff92dd6fa121784fcb104c56d7
bconstanzo <bconstanzo@users.noreply.github.com>
2020-07-24 02:31:45 -0300

    fix: bytes semantics in MBR parsing

--- haruspex/mbr.py
+++ haruspex/mbr.py
@@ -4,16 +4,16 @@ import struct
 class Record:
     """Handles one entry in the MBR table."""
     partition_types = {
-        b"\x00": "Empty",
-        b"\x01": "FAT12 CHS",
-        b"\x04": "FAT16 CHS",
-        b"\x05": "Microsoft Extended",
-        b"\x06": "FAT16 CHS 32MB",
-        b"\x07": "NTFS",
-        b"\x0b": "FAT32 CHS",
-        b"\x0c": "FAT32 LBA",
-        b"\x0e": "FAT16 LBA 2GB",
-        b"\x0f": "Microsoft Extended LBA",
+        0x00: "Empty",
+        0x01: "FAT12 CHS",
+        0x04: "FAT16 CHS",
+        0x05: "Microsoft Extended",
+        0x06: "FAT16 CHS 32MB",
+        0x07: "NTFS",
+        0x0b: "FAT32 CHS",
+        0x0c: "FAT32 LBA",
+        0x0e: "FAT16 LBA 2GB",
+        0x0f: "Microsoft Extended LBA",
     }   # this dict could be extended to include more types
 
     def __init__(self, data):
@@ -28,25 +28,24 @@ class Record:
         self.lba_start = 0x0
         self.size      = 0
         # after setting the attributes for the instance, we parse the raw data
-        self.parse()
+        self._parse()
     
     def __repr__(self):
         ret = "< Partition - {type} - boot: {bootable} @ {lba_start} of {size} >"
         return ret.format_map(self.__dict__)
     
-    def parse(self):
+    def _parse(self):
         """
         Parses self._raw_data and uses it to modifiy/complete the internal state
         of Record.
         """
         data = self._raw_data
-        self.bootable   = data[0] == b"\x80"
+        self.bootable   = data[0] == 0x80
         self.chs_start  = data[1:4]
-        self.type       = self.partition_types.get(data[4:5], "Unknown")
+        self.type       = self.partition_types.get(data[4], "Unknown")
         self.chs_end    = data[5:8]
         self.lba_start, = struct.unpack("<I", data[8:12])
-        self.size,      = struct.unpack("<i", data[12:16])
-        return data
+        self.size,      = struct.unpack("<I", data[12:16])
 
 
 class Table:

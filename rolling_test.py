cf3b0d96afc26748e574a17b26bdddffac1cdc4b
bconstanzo <bconstanzo@users.noreply.github.com>
2020-07-27 15:49:39 -0300

    added: fat32.FileHandle() setup for testing

--- rolling_test.py
+++ rolling_test.py
@@ -10,6 +10,15 @@ while data:
         break
     data = fd.read(512)
 record = haruspex.fat32.FileRecord(data[128:160])
+fh = haruspex.fat32.FileHandle(fs, record, "rb")
 
 print(mbr)
 print(fs)
+print(fh)
+
+# What we tested of FileHandle:
+# * Reading a specific file.
+# * Seeking and reading.
+# * Seeking past the end.
+# * Reading all and reading againg (empty)
+# * Seeking past the end and reading (empty)

30fa4c92991662de17cbdd920c661a69076f9a02
bconstanzo <bconstanzo@users.noreply.github.com>
2021-10-12 08:15:48 -0300

    add: ntfs import and corresponding version change

--- haruspex/__init__.py
+++ haruspex/__init__.py
@@ -3,8 +3,9 @@ from . import fat32
 from . import gpt
 from . import guid
 from . import mbr
+from . import ntfs
 from . import utils
 from . import vhd
 
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"

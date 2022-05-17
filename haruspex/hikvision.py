e2340e46321cc7a2ad8707b50ac7917c6c2dabbb
bconstanzo <bconstanzo@users.noreply.github.com>
2022-05-17 20:11:14 -0300

    add: init attributes, __repr__, and __str__ for the Hikvision class

--- haruspex/hikvision.py
+++ haruspex/hikvision.py
@@ -25,3 +25,38 @@ class Hikvision:
         self._base_address = base_address
         self._handle       = handle
         self._handle.seek(base_address)
+        master_sector = self._handle.read(512)
+        volsize, _, log_offset, log_size, _, data_offset, _, data_size, \
+            data_number, _, tree1_offset, tree1_size, _, tree2_offset,\
+            tree2_size = struct.unpack("<Q16sQQ8sQ8sQL4sQL4sQL", master_sector[72:72+108])
+        
+        self.volume_size  = volsize
+        self.log_offset   = log_offset
+        self.log_size     = log_size
+        self.data_offset  = data_offset
+        self.data_size    = data_size
+        self.data_number  = data_number
+        self.tree1_offset = tree1_offset
+        self.tree1_size   = tree1_size
+        self.tree2_offset = tree2_offset
+        self.tree2_size   = tree2_size
+        
+    def __repr__(self):
+        return f"< Hikvision DVR @ {self._base_address} >"
+    
+    def __str__(self):
+        return (
+               "< Hikvision DVR Filesystem:\n"
+               f"    {'base address':18}: {self._base_address}\n"
+               f"    {'volume size':18}: {self.volume_size}\n"
+               f"    {'log offset':18}: {self.log_offset}\n"
+               f"    {'log size':18}: {self.log_size}\n"
+               f"    {'data offset':18}: {self.data_offset}\n"
+               f"    {'data size':18}: {self.data_size}\n"
+               f"    {'# of data records':18}: {self.data_number}\n"
+               f"    {'tree1 offset':18}: {self.tree1_offset}\n"
+               f"    {'tree1 size':18}: {self.tree1_size}\n"
+               f"    {'tree2 offset':18}: {self.tree2_offset}\n"
+               f"    {'tree2 size':18}: {self.tree2_size}\n"
+               f">"
+        )
